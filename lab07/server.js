68fd57f560574af0ea23bc709db7ee67e6cf708d
Kirby <codemonkey941@gmail.com>
2016-10-18 12:31:49 -0400

    Halfway finish Lab 07. Update .gitignore file.

--- lab07/server.js
+++ lab07/server.js
@@ -1,14 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var server = express();
 
 const PORT = 3000;
 
-server.get('/', function(req, res) {
-	res.send('Hello, world!');
+server.use(express.static('public'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+
+server.get("/", function(req, res) {
+	res.send("Hello, world!");
 });
 
-server.use(express.static('public'));
+server.get("/hello", function(req, res) {
+	res.send({"message" : "Hello, AJAX"});
+});
 
 server.listen(PORT, function() {
 	console.log('I\'m listening on port 3000');
