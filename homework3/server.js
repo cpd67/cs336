fcaad4798f4ecbe6757bbc0738c08e4dd8a17643
Chris <cpd5@students.calvin.edu>
2016-11-12 20:56:51 -0500

    Update almost all routes. GET method that gets a single person based off of form data needs to be completed.

--- homework3/server.js
+++ homework3/server.js
@@ -7,6 +7,7 @@
  * CS-336 Lab 09 -
  * CS-336 Lab 10 - https://cs.calvin.edu/courses/cs/336/kvlinden/10mongo/lab.html
  * Mongodb tutorials: http://mongodb.github.io/node-mongodb-native/2.2/quick-start/
+ * Checking the size of a returned cursor of a find() call: https://docs.mongodb.com/v3.2/reference/method/cursor.toArray/
  */
 
 var express = require('express');
@@ -18,17 +19,10 @@ var databaseConnection;
 
 var app = express();
 
-//http://www.w3schools.com/js/js_arrays.asp
-//http://stackoverflow.com/questions/11427451/storing-json-value-in-javascript-array
-/*var data = [ { firstname: 'Sir Edmond', lastname: 'Jones', loginID: 1, startDate: '10/20/2000'},
-			 { firstname: 'David', lastname: 'Son', loginID: 2, startDate: '08/19/2010'},
-			 { firstname: 'Sarah', lastname: 'Smiles', loginID: 3, startDate: '10/3/2015'} ];
-*/
-
 ///////////////Homework 3////////////////////
 //Adapted from: https://expressjs.com/en/guide/database-integration.html#mongo
 //Establish a connection with the database, then store the connection in a global variable.
-MongoClient.connect('mongodb://cs336:PASSWORD@ds149577.mlab.com:49577/homework3', function (err, db) {
+MongoClient.connect('mongodb://cs336:bjarne@ds149577.mlab.com:49577/homework3', function (err, db) {
   if (err) throw err
 
 	databaseConnection = db;
@@ -45,49 +39,65 @@ app.use(function(req, res, next) {
     // an API server in conjunction with something like webpack-dev-server.
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Disable caching so we'll always get the latest comments.
+    // Disable caching so we'll always get the latest people.
     res.setHeader('Cache-Control', 'no-cache');
     next();
 });
 
 //List all people objects (/people)
 app.get('/people', function(req, res) {
+  //Get a handle to the 'people' collection.
 	var collection = databaseConnection.collection('people');
 
+  //Get all of the people from the collection.
 	collection.find({}).toArray(function(err, docs) {
 		if(err) throw err;
-
+    //Send the data
 		res.json(docs);
 	});
 });
 
 //Get the full record for the person with the given ID (/person/id)
 app.get('/person/:id', function(req, res) {
-	var id = req.params.id;
+
+  //https://coderwall.com/p/5tlhmw/converting-strings-to-number-in-javascript-pitfalls
+  //ID number is always a String for some odd reason,
+  //so change into a Number.
+	var id = Number(req.params.id);
 
 	var collection = databaseConnection.collection('people');
 
-	//Might have to do findOne()...
-	collection.find({'loginID': id}).toArray(function(err, docs) {
-		if(err) throw err;
-
-		res.json(docs);
-	});
+  //We'll have to check if the find() call returns a cursor with size 0.
+  var holder = collection.find({loginID: id});
+  holder.toArray(function(err, docs) {
+    //Did we get no documents?
+    if(docs.length == 0) {
+      //Yep!
+      res.sendStatus(404);
+    } else {
+      //No. Send the data.
+      res.json(docs);
+    }
+  });
 });
 
 //Get the full name (first and last) for the person with the given ID (/person/id/name)
 app.get('/person/:id/name', function(req, res) {
-	var id = req.params.id;
+	var id = Number(req.params.id);
 
 	var collection = databaseConnection.collection('people');
 
-	//Might have to do findOne()...
-	collection.find({'loginID': id}).toArray(function(err, docs) {
-		if(err) throw err;
-
-		res.send(docs.value.firstname + " " + docs.value.lastname);
-	});
-
+  var holder = collection.find({loginID: id});
+  holder.toArray(function(err, docs) {
+    //Did we find no one?
+    if(docs.length == 0) {
+      //Yep!
+      res.sendStatus(404);
+    } else {
+      //No. Send the full name.
+      res.send(docs[0].firstname + " " + docs[0].lastname);
+    }
+  });
 });
 
 //Seniority (number of years with the organization) of the person with the given ID (/person/id/years)
@@ -95,36 +105,32 @@ app.get('/person/:id/name', function(req, res) {
 //for the years worked
 app.get('/person/:id/years', function(req, res) {
 	//Get today's date
-/*	var todayDate = new Date();
-	var startDate, calcDate, years;
-	//Iterate through the dataset
-	for(var i = 0; i < data.length; i++) {
-		//If the login ID equals the one passed...
-		if(data[i].loginID == req.params.id) {
-			//Store the start date of the person
-			startDate = data[i].startDate;
-		}
-	}
+  var todayDate = new Date();
+  var startDate, calcDate, years;
 
-	//If we haven't found the start date...
-	if(startDate == null) {
-		res.sendStatus(404);
-	} else {
-		//Calculate the years worked
-		calcDate = new Date(startDate);
-		years = todayDate.getFullYear() - calcDate.getFullYear();
-		var month = todayDate.getMonth() - calcDate.getMonth();
-		if (month < 0 || (month === 0 && todayDate.getDate() < calcDate.getDate())) {
-        	years--;
-    	}
-		//http://www.w3schools.com/jsref/jsref_tostring_number.asp
-		//Send the result
-		res.send("Years: " + years.toString());
-	}
-	*/
+  var id = Number(req.params.id);
+
+  var collection = databaseConnection.collection('people');
+
+  //Check if the find() call returns nothing.
+  var holder = collection.find({loginID: id});
+  holder.toArray(function(err, docs) {
+    if(docs.length == 0) {
+      res.sendStatus(404);
+    } else {
+      //Calculate the number of years worked.
+      startDate = docs[0].startDate;
+      calcDate = new Date(startDate);
+      years = todayDate.getFullYear() - calcDate.getFullYear();
+      var month = todayDate.getMonth() - calcDate.getMonth();
+      if (month < 0 || (month === 0 && todayDate.getDate() < calcDate.getDate())) {
+        years--;
+      }
+      //Send the years.
+      res.send("Years: " + years.toString());
+    }
+  });
 });
-
-////////////////Homework 2/////////////////////
 
 //GET method that retrieves a person from our database
 //and used in tandem with the AJAX call of the second new webpage
@@ -151,10 +157,11 @@ app.get('/person', function(req, res) {
 
 //POST method that creates new people
 app.post('/people', function(req, res) {
+
 	//Get the passed form data
 	var first = req.body.firstname;
 	var last = req.body.lastname;
-	var login = req.body.loginID;
+	var login = Number(req.body.loginID);
 	var date = req.body.startDate;
 
 	//If some data is missing...
@@ -167,78 +174,74 @@ app.post('/people', function(req, res) {
 	}
 
 	//Check if the login ID is already in the "database"
-	for(var i = 0; i < data.length; i++) {
-		if(data[i].loginID == login) {
-			//http://stackoverflow.com/questions/25541203/http-response-code-when-resource-creation-post-fails-due-to-existing-matching-re?noredirect=1&lq=1
-			//The user already exists, send a conflict status code.
-			console.log("Person already exists with that login ID!");
-			console.log(data[i]);
-			res.sendStatus(409);
-			return;
-		}
-	}
-
 	//Everything seems okay. Go ahead with the addition of the person.
 	var newPerson = { firstname: first, lastname: last, loginID: login, startDate: date};
 
+  //We'll have to check if the Person is already in the database...
 	//Get the collection of People from the database
 	var collection = databaseConnection.collection('people');
 
-	//Insert the new Person
-	collection.insert(newPerson);
-//	res.json(data); //I didn't say res.json, which was why I was geting that strange error...
+  var holder = collection.find({loginID: login});
+
+  //Check if the Person is already in the database.
+  holder.toArray(function(err, docs) {
+    //Yes.
+    if(docs.length > 0) {
+      console.log("That Person already exists!");
+      console.log(docs[0]);
+    //No.
+    } else {
+      //Insert the new Person
+      collection.insert(newPerson);
+    }
+  });
 });
 
 
 //http://stackoverflow.com/questions/12142652/what-is-the-usefulness-of-put-and-delete-http-request-methods
 //PUT method that updates the first name of a person (tested with curl)
 app.put('/person/:id/:first', function(req, res) {
-/*	var person, index;
-	//Get the first name of the person
-	var newName = req.params.first;
-	for(var i = 0; i < data.length; i++) {
-		if(data[i].loginID == req.params.id) {
-			person = data[i];
-			index = i;
-		}
-	}
+  //Get the first name of the person
+  var id = Number(req.params.id);
+  var newName = req.params.first;
 
-	//Is the person in the database?
-	if(person == null) {
-		//Not found!
-		res.sendStatus(404);
-	} else {
-		//Found!
-		data[index].firstname = newName;
-		res.sendStatus(201);
-	}
-	*/
+  var collection = databaseConnection.collection('people');
+
+  //We need to check if the Person is even in the database
+  var holder = collection.find({loginID: id});
+
+  holder.toArray(function(err, docs) {
+    //Did we find the Person?
+    if(docs.length == 0) {
+      //Nope!
+      res.sendStatus(404);
+    } else {
+      //Yes. Update the name.
+      collection.update({loginID: id}, { $set: {firstname: newName } });
+      res.sendStatus(201);
+    }
+  });
 });
 
 //DELETE method that removes a person from our "database" (tested with curl)
 app.delete('/person/:id', function(req, res) {
-/*	var person, index;
-	for(var i = 0; i < data.length; i++) {
-		if(data[i].loginID == req.params.id) {
-			person = data[i];
-			index = i;
-		}
-	}
+  var id = Number(req.params.id);
 
-	//Did we find the person?
-	if(person == null) {
-		//Nope!
-		res.sendStatus(404);
-	} else {
-		//http://stackoverflow.com/questions/5767325/how-to-remove-a-particular-element-from-an-array-in-javascript
-		//Found! Remove them from the "database"
-		data.splice(index, 1);
-		res.sendStatus(200);
-	}
-	*/
+  var collection = databaseConnection.collection('people');
+
+  var holder = collection.find({loginID: id});
+
+  holder.toArray(function(err, docs) {
+    if(docs.length == 0) {
+      res.sendStatus(404);
+    } else {
+      collection.remove({loginID: id});
+      res.sendStatus(200);
+    }
+  });
 });
 
 //Listen on port 3000
 app.listen(3000, function () {
-        console.log('I\'m listening on port 3000!');
+  console.log('I\'m listening on port 3000!');
 });
