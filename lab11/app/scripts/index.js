61acc4f5f4a71c9faa898f7a6ed8dcf4417cbe99
Kirby <codemonkey941@gmail.com>
2016-11-16 10:54:56 -0500

    Start Lab 11.

---
+++ lab11/app/scripts/index.js
@@ -0,0 +1,17 @@
+/** Copied over from the Facebook tutorial, as for some reason my old one
+    was not working correctly.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Remarkable from 'remarkable';
+import $ from 'jquery';
+
+import '../css/base.css';
+
+import CommentBox from './CommentBox.js';
+
+ReactDOM.render(
+  <CommentBox url="/api/comments" pollInterval={2000} />,
+  document.getElementById('content')
+);
