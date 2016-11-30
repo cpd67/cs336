be86906f4c555563e5238505ca4fecec02a83398
Kirby <codemonkey941@gmail.com>
2016-11-30 11:25:43 -0500

    Start Lab 13.

---
+++ app/scripts/index.js
@@ -0,0 +1,20 @@
+/** Copied over from the Facebook tutorial, as for some reason my old one
+    was not working correctly.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Router, Route, Redirect, browserHistory } from 'react-router';
+
+import CommentBox from './CommentBox.js';
+import CommentEdit from './commentEdit.js';
+
+import '../css/base.css';
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={CommentBox}/>
+    <Route path="/:id" component={CommentEdit}/>
+  </Router>
+), document.getElementById('content'));
